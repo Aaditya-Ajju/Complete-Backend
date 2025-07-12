a5914dd145eca6850c35c8bdb25569b178d6baa6
Aaditya Raj <9922008359@klu.ac.in>
2025-07-12 17:31:56 +0530

    Utils Discussion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 
+import app from "./app.js";
 import dotenv from "dotenv"
 import connectDb from "./db/index.js";
 dotenv.config({ quiet:true});
 
 
 connectDb()
+.then(()=>{
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+})
+.catch((error)=>{
+  console.error("Failed to connect to the database:", error);
+  
+})
 
 
 
